4fe3a3b380223769ca67eb9f533e413b440e7097
Michal Domagała <maxisu1708@Interia.pl>
2026-06-03 14:56:27 +0200

    First_project_p8

--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ class Settings:
         self.fleet_direction = 1
 
         self.bullet_speed = 5.0
-        self.bullet_width = 5
+        self.bullet_width = 5000
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
         self.bullets_allowed = 3
